ba798489e9fcc6b5244c3fc39b522a99d411c199
Mihkel <viilveer@users.noreply.github.com>
2018-04-17 16:32:32 +0300

    edited skill calculation test code style

--- tests/unit/models/EmployeeSkillCalculationTest.php
+++ tests/unit/models/EmployeeSkillCalculationTest.php
@@ -40,7 +40,10 @@ class EmployeeSkillCalculationTest extends \Codeception\Test\Unit
             new Skill(['id' => 2, 'name' => 'Test Skill 2', 'parent_skill_id' => 1])
         ];
 
-        $employeeSkills = [new EmployeeSkill(['skill_id' => 2, 'level' => 4]), new EmployeeSkill(['skill_id' => 1, 'level' => 4])];
+        $employeeSkills = [
+            new EmployeeSkill(['skill_id' => 2, 'level' => 4]),
+            new EmployeeSkill(['skill_id' => 1, 'level' => 4])
+        ];
         $calculator = new EmployeeSkillCalculator($skills, $employeeSkills);
         expect($calculator->getEmployeeCalculatedSkills())->count(0);
     }
